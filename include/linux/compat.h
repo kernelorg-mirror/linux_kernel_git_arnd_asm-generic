856f1f9b12ee8ce11a4d31dc8d275dceef92a268
Arnd Bergmann <arnd@arndb.de>
2020-09-11 15:17:19 +0200

    net: socket: rework SIOC?IFMAP ioctls
    SIOCGIFMAP and SIOCSIFMAP currently require compat_alloc_user_space()
    and copy_in_user() for compat mode.
    
    Move the compat handling into the location where the structures are
    actually used, to avoid using those interfaces and get a clearer
    implementation.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    ---
    changes in v3:
     - complete rewrite
    
    changes in v2:
     - fix building with CONFIG_COMPAT disabled (0day bot)
     - split up dev_ifmap() into more readable helpers (hch)
     - move rcu_read_unlock() for readability (hch)

--- include/linux/compat.h
+++ include/linux/compat.h
@@ -95,6 +95,15 @@ struct compat_iovec {
 	compat_size_t	iov_len;
 };
 
+struct compat_ifmap {
+	compat_ulong_t mem_start;
+	compat_ulong_t mem_end;
+	unsigned short base_addr;
+	unsigned char irq;
+	unsigned char dma;
+	unsigned char port;
+};
+
 #ifdef CONFIG_COMPAT
 
 #ifndef compat_user_stack_pointer
@@ -314,15 +323,6 @@ typedef struct compat_sigevent {
 		} _sigev_thread;
 	} _sigev_un;
 } compat_sigevent_t;
-
-struct compat_ifmap {
-	compat_ulong_t mem_start;
-	compat_ulong_t mem_end;
-	unsigned short base_addr;
-	unsigned char irq;
-	unsigned char dma;
-	unsigned char port;
-};
 
 struct compat_if_settings {
 	unsigned int type;	/* Type of physical device or protocol */
