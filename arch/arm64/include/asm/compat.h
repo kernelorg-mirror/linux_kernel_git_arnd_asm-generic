2924197946d7f95ba52a493397a11a1f969f4aa8
Arnd Bergmann <arnd@arndb.de>
2020-09-16 17:21:03 +0200

    arch: remove compat_alloc_user_space
    All users of compat_alloc_user_space() and copy_in_user() have been
    removed from the kernel, only a few functions in sparc remain that can
    be changed to calling arch_copy_in_user() instead.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm64/include/asm/compat.h
+++ arch/arm64/include/asm/compat.h
@@ -115,11 +115,6 @@ typedef u32		compat_sigset_word;
 #define compat_user_stack_pointer() (user_stack_pointer(task_pt_regs(current)))
 #define COMPAT_MINSIGSTKSZ	2048
 
-static inline void __user *arch_compat_alloc_user_space(long len)
-{
-	return (void __user *)compat_user_stack_pointer() - len;
-}
-
 struct compat_ipc64_perm {
 	compat_key_t key;
 	__compat_uid32_t uid;
