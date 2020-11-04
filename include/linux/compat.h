a758bee1e370a5c632dc300eab100a349c684f40
Arnd Bergmann <arnd@arndb.de>
2020-11-04 20:50:41 +0100

    net: socket: rework compat_ifreq_ioctl()
    compat_ifreq_ioctl() is one of the last users of copy_in_user() and
    compat_alloc_user_space(), as it attempts to convert the 'struct ifreq'
    arguments from 32-bit to 64-bit format as used by dev_ioctl() and a
    couple of socket family specific interpretations.
    
    The current implementation works correctly when calling dev_ioctl(),
    inet_ioctl(), ieee802154_sock_ioctl(), atalk_ioctl(), qrtr_ioctl()
    and packet_ioctl(). The ioctl handlers for x25, netrom, rose and x25 do
    not interpret the arguments and only block the corresponding commands,
    so they do not care.
    
    For af_inet6 and af_decnet however, the compat conversion is slightly
    incorrect, as it will copy more data than the native handler accesses,
    both of them use a structure that is shorter than ifreq.
    
    Replace the copy_in_user() conversion with a pair of accessor functions
    to read and write the ifreq data in place with the correct length where
    needed, while leaving the other ones to copy the (already compatible)
    structures directly.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/compat.h
+++ include/linux/compat.h
@@ -109,6 +109,33 @@ struct compat_ifconf {
 	compat_uptr_t	ifcbuf;
 };
 
+struct compat_if_settings {
+	unsigned int type;	/* Type of physical device or protocol */
+	unsigned int size;	/* Size of the data allocated by the caller */
+	compat_uptr_t ifs_ifsu;	/* union of pointers */
+};
+
+struct compat_ifreq {
+	union {
+		char	ifrn_name[IFNAMSIZ];    /* if name, e.g. "en0" */
+	} ifr_ifrn;
+	union {
+		struct	sockaddr ifru_addr;
+		struct	sockaddr ifru_dstaddr;
+		struct	sockaddr ifru_broadaddr;
+		struct	sockaddr ifru_netmask;
+		struct	sockaddr ifru_hwaddr;
+		short	ifru_flags;
+		compat_int_t	ifru_ivalue;
+		compat_int_t	ifru_mtu;
+		struct	compat_ifmap ifru_map;
+		char	ifru_slave[IFNAMSIZ];   /* Just fits the size */
+		char	ifru_newname[IFNAMSIZ];
+		compat_uptr_t	ifru_data;
+		struct	compat_if_settings ifru_settings;
+	} ifr_ifru;
+};
+
 #ifdef CONFIG_COMPAT
 
 #ifndef compat_user_stack_pointer
@@ -328,32 +355,6 @@ typedef struct compat_sigevent {
 		} _sigev_thread;
 	} _sigev_un;
 } compat_sigevent_t;
-
-struct compat_ifreq {
-	union {
-		char	ifrn_name[IFNAMSIZ];    /* if name, e.g. "en0" */
-	} ifr_ifrn;
-	union {
-		struct	sockaddr ifru_addr;
-		struct	sockaddr ifru_dstaddr;
-		struct	sockaddr ifru_broadaddr;
-		struct	sockaddr ifru_netmask;
-		struct	sockaddr ifru_hwaddr;
-		short	ifru_flags;
-		compat_int_t	ifru_ivalue;
-		compat_int_t	ifru_mtu;
-		struct	compat_ifmap ifru_map;
-		char	ifru_slave[IFNAMSIZ];   /* Just fits the size */
-		char	ifru_newname[IFNAMSIZ];
-		compat_caddr_t	ifru_data;
-		struct	compat_if_settings ifru_settings;
-	} ifr_ifru;
-};
-
-struct compat_ifconf {
-	compat_int_t	ifc_len;                /* size of buffer */
-	compat_caddr_t  ifcbuf;
-};
 
 struct compat_robust_list {
 	compat_uptr_t			next;
