c8f12d0ffe3a233fa1e088ded52e8dac2d2ef4aa
Arnd Bergmann <arnd@arndb.de>
2024-06-05 18:17:27 +0200

    openrisc: fix __sys_clone prototype
    I noticed this while working on type checking. The prototype is not
    used anywhere, but let's make this match the sys_clone version anyway.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/openrisc/include/asm/syscalls.h
+++ arch/openrisc/include/asm/syscalls.h
@@ -21,7 +21,7 @@ asmlinkage long sys_or1k_atomic(unsigned long type, unsigned long *v1,
 #include <asm-generic/syscalls.h>
 
 asmlinkage long __sys_clone(unsigned long clone_flags, unsigned long newsp,
-			void __user *parent_tid, void __user *child_tid, int tls);
+			int __user *parent_tid, int __user *child_tid, unsigned long tls);
 asmlinkage long __sys_clone3(struct clone_args __user *uargs, size_t size);
 asmlinkage long __sys_fork(void);
 
