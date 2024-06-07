f73cf2253b730bb9caa984a452261d500f0af271
Arnd Bergmann <arnd@arndb.de>
2024-06-07 08:04:16 +0200

    s390: use generic syscall table generation
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/s390/include/asm/unistd.h
+++ arch/s390/include/asm/unistd.h
@@ -8,7 +8,6 @@
 #define _ASM_S390_UNISTD_H_
 
 #include <uapi/asm/unistd.h>
-#include <asm/unistd_nr.h>
 
 #define __ARCH_WANT_NEW_STAT
 #define __ARCH_WANT_OLD_READDIR
@@ -35,5 +34,7 @@
 #define __ARCH_WANT_SYS_FORK
 #define __ARCH_WANT_SYS_VFORK
 #define __ARCH_WANT_SYS_CLONE
+
+#define NR_syscalls __NR_syscalls
 
 #endif /* _ASM_S390_UNISTD_H_ */
